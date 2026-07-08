94959356e0863733dc6e5083f397c9911112d892
mfobbsie <mfobbsguillory@gmail.com>
2026-07-08 10:29:53 -0700

    Update app-frontend/src/components/NavBar.tsx
    Co-authored-by: James Hightower <jamesehightower@gmail.com>

--- app-frontend/src/components/NavBar.tsx
+++ app-frontend/src/components/NavBar.tsx
@@ -14,7 +14,6 @@ export const NavBar = (): ReactNode => {
       }
     };
 
-    window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, [isOpen]);
 
